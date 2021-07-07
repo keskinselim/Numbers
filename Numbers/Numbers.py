d6071631fdc420e398067b3d6352c23050f47d0a
ryosh <ryosh@DESKTOP-CO7L1E0>
2021-07-07 16:28:51 +0200

    Proje dosyası ekle.

---
+++ Numbers/Numbers.py
@@ -0,0 +1,26 @@
+25*25
+
+#This is correct but you can not anything like this if you want to see it you should print it 
+
+print(25*25)
+
+# or
+
+number=25*25
+print(number)
+
+#in python you can use " + " , " - " , " * " , " / "
+
+print(25+25)
+print(25-5)
+print(12*12)
+print(12/4)
+
+#you can check it type 
+
+type(34657)
+
+#this is int but if you write in " " its gonna be string
+
+type("34657")
+
